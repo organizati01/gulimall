136cc641d1b9fee465da9ad37f2b8d566fe376e9
ideapad300-win7 <361763560@qq.com>
2021-01-05 22:54:28 +0800

    finished 18

--- gulimall-product/src/main/java/com/atguigu/gulimall/product/GulimallProductApplication.java
+++ gulimall-product/src/main/java/com/atguigu/gulimall/product/GulimallProductApplication.java
@@ -1,8 +1,22 @@
 package com.atguigu.gulimall.product;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 整合mybatis-plus
+ * 1.导入依赖
+ * 2.配置
+ *  2.1.配置数据源
+ *   2.1.1导入数据库驱动
+ *   2.1.2application.yml配置相关信息
+ *  2.2.配置mybatis-plus
+ *   2.2.1.使用MapperScan注解配置扫描路径
+ *   2.2.1.配置mapper映射文件路径 mybatis-plus.mapper-locations: classpath*:/mapper/** /*.xml,
+		classpath*表示扫描所有类路径(包括其他jar包的类路径)
+ */
+@MapperScan("com.atguigu.gulimall.product.dao")
 @SpringBootApplication
 public class GulimallProductApplication {
 
--- gulimall-product/src/test/java/com/atguigu/gulimall/product/GulimallProductApplicationTests.java
+++ gulimall-product/src/test/java/com/atguigu/gulimall/product/GulimallProductApplicationTests.java
@@ -1,13 +1,41 @@
 package com.atguigu.gulimall.product;
 
+import com.atguigu.gulimall.product.entity.BrandEntity;
+import com.atguigu.gulimall.product.service.BrandService;
+
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Iterator;
+import java.util.List;
 
 @SpringBootTest
 class GulimallProductApplicationTests {
 
+	@Autowired
+	BrandService brandService;
+
 	@Test
 	void contextLoads() {
+		BrandEntity brandEntity = new BrandEntity();
+//		brandEntity.setName("华为");
+//		brandService.save(brandEntity);
+
+//		brandEntity.setBrandId(1L);
+//		brandEntity.setName("小米");
+//		brandEntity.setDescript("小米品牌");
+//		brandService.updateById(brandEntity);
+
+		List<BrandEntity> brand_id = brandService.list(new QueryWrapper<BrandEntity>().eq("brand_id", 1L));
+		Iterator<BrandEntity> iterator = brand_id.iterator();
+		while (iterator.hasNext()){
+			BrandEntity next = iterator.next();
+			System.out.println(next);
+		}
+		System.out.println("保存成功");
 	}
 
 }
